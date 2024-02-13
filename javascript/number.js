1988747fc58f7f88d31bc805184636fabea4d2ce
Zabiullah Shah <152472366+Zabiullah-05@users.noreply.github.com>
2024-02-13 11:21:49 +0000

    some edits

--- javascript/number.js
+++ javascript/number.js
@@ -1,8 +1,8 @@
-const num= 300.588;
+const num= 3000000;
 console.log(num)
 console.log(typeof num)
 console.log(num.toString())
-console.log(num.length);
+console.log(num.toString().length);
 console.log(num.toFixed(1))
 console.log(num.toExponential())
 console.log(num.toLocaleString());
