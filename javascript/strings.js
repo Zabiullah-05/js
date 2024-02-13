a14198f24336ef12e8c6ff8687715cda305d5091
Zabiullah Shah <152472366+Zabiullah-05@users.noreply.github.com>
2024-02-13 11:01:30 +0000

    strings and methods

---
+++ javascript/strings.js
@@ -0,0 +1,15 @@
+const firstname='ali'
+const lastname=' hassan'
+const name= firstname+lastname;
+//  console.log(firstname+lastname)
+// console.log(`Hello my name is ${firstname+lastname}`)
+// console.log(`Hello my name is ${firstname.concat(lastname)}`)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.charAt(2))
+console.log(name.indexOf('l'))
+console.log(name.length)
+console.log(name.slice(0,3))
+console.log(firstname.concat(lastname))
+console.log(name.replace('a','A'))
+console.log(name.split(" "))
