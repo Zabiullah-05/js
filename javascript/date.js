90b638d87cacac58194fb129bc6a66ad95e44815
Zabiullah Shah <152472366+Zabiullah-05@users.noreply.github.com>
2024-02-13 11:39:54 +0000

    date and methods

---
+++ javascript/date.js
@@ -0,0 +1,13 @@
+const myDate= new Date()
+console.log(myDate);
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getFullYear())
+console.log(myDate.getMonth());
+console.log(myDate.getTime());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getMilliseconds());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+
