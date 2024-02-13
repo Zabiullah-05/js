661a4f9bb66e1b5efd61f5713bb33bfc729d5330
Zabiullah Shah <152472366+Zabiullah-05@users.noreply.github.com>
2024-02-13 11:19:16 +0000

    number methods

--- javascript/number.js
+++ javascript/number.js
@@ -1,6 +1,9 @@
-const num= 30;
+const num= 300.588;
 console.log(num)
 console.log(typeof num)
-console.log(num.toString().length)
+console.log(num.toString())
+console.log(num.length);
 console.log(num.toFixed(1))
 console.log(num.toExponential())
+console.log(num.toLocaleString());
+console.log(num.toPrecision(2));
