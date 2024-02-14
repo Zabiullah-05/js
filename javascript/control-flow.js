ecc6300ed485a687fe7053ee46c398d6e3acd4ae
Zabiullah Shah <152472366+Zabiullah-05@users.noreply.github.com>
2024-02-14 12:21:57 +0000

    conditionals

---
+++ javascript/control-flow.js
@@ -0,0 +1,42 @@
+// structure of if else statement
+
+
+// if(condition:true){
+//     //executed
+// }else{ //condition:false
+//     //executed
+// }
+        
+            //  if else statement with example
+let a=12;
+if(a<=10){
+    // if condtion is true the code executed
+    console.log('less than')
+}else{
+        // if condtion is false the code executed
+    console.log('greater than')
+}
+
+                //    if else-if else statement with example
+    // structure 
+    
+    // if(condition:true) {
+        //execute code
+    // }
+    // else if(condition:ture){
+        //execute code
+    // }else{
+        //execute code
+    // }
+
+
+    //example
+
+    let weather="cloudy"
+    if(weather==="sunny"){
+        console.log("temperature warm")
+    }else if(weather==="rainny"){
+        console.log('you need umbralla')
+    }else{
+        console.log('go to home');
+    }
