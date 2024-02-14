eb09133e6faac91da2cdf5f61fa2cc22a9a76c4a
Zabiullah Shah <152472366+Zabiullah-05@users.noreply.github.com>
2024-02-14 12:29:58 +0000

    switch statement

---
+++ javascript/switch.js
@@ -0,0 +1,20 @@
+                                            // switch statement
+                                            // structure
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }                                            
+
+let color='black';
+switch (true) {
+    case (color==='red'):
+        console.log('color is red')
+        break;
+    default:
+        console.log('not a red color')
+        break;
+}
